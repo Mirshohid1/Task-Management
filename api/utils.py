c821b39a232758c80dd92f9137eaedf6f3527720
l0n3r <mirshohid1214@gmail.com>
2024-12-16 01:02:28 +0500

    create func decode_jwt in utils.py

--- api/utils.py
+++ api/utils.py
@@ -2,6 +2,9 @@ import jwt
 from datetime import datetime, timedelta
 
 from django.conf import settings
+from django.core.exceptions import ObjectDoesNotExist
+from rest_framework.exceptions import AuthenticationFailed
+from django.contrib.auth.models import User
 
 
 def generate_jwt(user):
@@ -15,3 +18,21 @@ def generate_jwt(user):
     token = jwt.encode(payload, settings.SECRET_KEY, algorithm='HS256')
 
     return token
+
+def decode_jwt(token):
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=['HS256'])
+        user_id = payload.get('id')
+        if not user_id:
+            raise AuthenticationFailed("Token payload missing user ID")
+    except jwt.ExpiredSignatureError:
+        raise AuthenticationFailed("Token has expired")
+    except jwt.InvalidTokenError:
+        raise AuthenticationFailed("Invalid token")
+
+    try:
+        user = User.objects.get(id=user_id)
+    except ObjectDoesNotExist:
+        raise AuthenticationFailed("User does not exist")
+
+    return user
