6ec30abfded7d1c6ae7595d7d17baf29dd2d1ccf
l0n3r <mirshohid1214@gmail.com>
2024-12-15 16:35:16 +0500

    created serializers TaskSerializer and TaskInputSerializer

---
+++ api/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from .models import Task
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = (
+            'id', 'title',
+            'description', 'status',
+            'created_at', 'owner',
+        )
+
+
+class TaskInputSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = ('title', 'description', 'status')
