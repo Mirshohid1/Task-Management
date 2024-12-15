e7af0887d06bbbd2332afd6e74c84f2ae10ee5db
l0n3r <mirshohid1214@gmail.com>
2024-12-15 15:50:21 +0500

    installed_apps set up

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # third-party apps
+    'rest_framework',
+    'drf_spectacular',
+
+    # local apps
+    'api',
 ]
 
 MIDDLEWARE = [
